32d8ec4761b18f3b8840f1ea6f71df716981a721
Huy Nguyen <luonghuy0406@gmail.com>
2023-04-07 17:07:16 +0700

    Updates

---
+++ precache-manifest.8900e96622d60699e8f86197c3b27a93.js
@@ -0,0 +1,290 @@
+self.__precacheManifest = [
+  {
+    "revision": "6653c7e16ec795c3b9f9",
+    "url": "./static/css/main.b4ac8e2d.chunk.css"
+  },
+  {
+    "revision": "6653c7e16ec795c3b9f9",
+    "url": "./static/js/main.b8a88f17.chunk.js"
+  },
+  {
+    "revision": "6c65847982729a77b485",
+    "url": "./static/js/runtime~main.3cc2006c.js"
+  },
+  {
+    "revision": "6acc7d4ed00337a08eff",
+    "url": "./static/css/2.71c954ac.chunk.css"
+  },
+  {
+    "revision": "6acc7d4ed00337a08eff",
+    "url": "./static/js/2.d080bae6.chunk.js"
+  },
+  {
+    "revision": "05115d069817e63d83ac",
+    "url": "./static/js/3.8c3e505c.chunk.js"
+  },
+  {
+    "revision": "ae2ef61b11c73c36d1c732a59490dc15",
+    "url": "./static/media/MIR.ae2ef61b.png"
+  },
+  {
+    "revision": "278cceaade526a6f2645c4bbc687227e",
+    "url": "./static/media/SELL.278cceaa.png"
+  },
+  {
+    "revision": "ed44266a4e097474b03e3f45483cede4",
+    "url": "./static/media/RENTAL.ed44266a.png"
+  },
+  {
+    "revision": "deee6b49eb5b3af4e7e29f53c5285c42",
+    "url": "./static/media/OTHER.deee6b49.png"
+  },
+  {
+    "revision": "2b2b7da40e9fe37746226683d444215a",
+    "url": "./static/media/getFetch.2b2b7da4.cjs"
+  },
+  {
+    "revision": "dcc2dcc94b0041a0d26660fe382f315a",
+    "url": "./static/media/aboutus.dcc2dcc9.png"
+  },
+  {
+    "revision": "536fb5fefd6a82c9fbbc6811a4eb27c1",
+    "url": "./static/media/banner.536fb5fe.mp4"
+  },
+  {
+    "revision": "04eb8fc57f27498e5ae37523e3bfb2c7",
+    "url": "./static/media/revicons.04eb8fc5.woff"
+  },
+  {
+    "revision": "2feb69ccb596730c72920c6ba3e37ef8",
+    "url": "./static/media/revicons.2feb69cc.eot"
+  },
+  {
+    "revision": "17629a5dfe0d3c3946cf401e1895f091",
+    "url": "./static/media/revicons.17629a5d.ttf"
+  },
+  {
+    "revision": "ed44c7222ce06cfe347cc662a91126fe",
+    "url": "./static/media/Ourmission.ed44c722.png"
+  },
+  {
+    "revision": "fd70e6bfa2f6a5baf8ee8d76d503a585",
+    "url": "./static/media/cat_pumps.fd70e6bf.png"
+  },
+  {
+    "revision": "649c5883f68e10cbd8cf9bc2e49027eb",
+    "url": "./static/media/hydraulics_international.649c5883.png"
+  },
+  {
+    "revision": "054d13777dfa0d40b351f3d1cb4a6351",
+    "url": "./static/media/graphic.054d1377.png"
+  },
+  {
+    "revision": "c64af6b8c8ff11c3a1cf53c5ae29bf64",
+    "url": "./static/media/norriseal.c64af6b8.png"
+  },
+  {
+    "revision": "e10850c6e49d4fda66dd30aabc4a7a28",
+    "url": "./static/media/cat_pump.e10850c6.png"
+  },
+  {
+    "revision": "3bb26a447af97fbf7680f8643139e860",
+    "url": "./static/media/HII.3bb26a44.png"
+  },
+  {
+    "revision": "5b11a83c38fff40ef20dee6213b4211b",
+    "url": "./static/media/Techcal.5b11a83c.png"
+  },
+  {
+    "revision": "337b8a4d627f43c3b1e367a75dcac739",
+    "url": "./static/media/graphic.337b8a4d.png"
+  },
+  {
+    "revision": "059d227cb982ef3ecac7f5999126dd08",
+    "url": "./static/media/Norriseal.059d227c.png"
+  },
+  {
+    "revision": "2c5346d1a8a01746fd266b29fb2f03f1",
+    "url": "./static/media/l1_1.2c5346d1.png"
+  },
+  {
+    "revision": "ad964ffe6266ce522af27571da34e843",
+    "url": "./static/media/l1_2.ad964ffe.png"
+  },
+  {
+    "revision": "659da5f181fb129f4c16296fe09f1052",
+    "url": "./static/media/l1_3.659da5f1.png"
+  },
+  {
+    "revision": "53f4379405d3011e026c2658dd2fee39",
+    "url": "./static/media/l1_4.53f43794.png"
+  },
+  {
+    "revision": "37dac5b34e3bd17a130b195664e7e5c8",
+    "url": "./static/media/l2_1.37dac5b3.png"
+  },
+  {
+    "revision": "c278767469c9d137aa20d6551b546e0e",
+    "url": "./static/media/l2_2.c2787674.png"
+  },
+  {
+    "revision": "a5861c814e539525027c84db368a9a11",
+    "url": "./static/media/l2_3.a5861c81.png"
+  },
+  {
+    "revision": "c7d7d5aa71d5277b76f64fde4554f5af",
+    "url": "./static/media/l2_4.c7d7d5aa.png"
+  },
+  {
+    "revision": "4f253e03cbea807aefbc4114ead12b85",
+    "url": "./static/media/l2_5.4f253e03.png"
+  },
+  {
+    "revision": "6b403604d57b1fef5bd482fead950878",
+    "url": "./static/media/l3_1.6b403604.png"
+  },
+  {
+    "revision": "d6c6dd7f1f3821d1db19b8c0fba6a742",
+    "url": "./static/media/l3_3.d6c6dd7f.png"
+  },
+  {
+    "revision": "cf2cfa59eb8d3980978050a632b3fa9c",
+    "url": "./static/media/l3_4.cf2cfa59.png"
+  },
+  {
+    "revision": "6e94ae91267a80bc5469d8946970a5d6",
+    "url": "./static/media/l3_5.6e94ae91.png"
+  },
+  {
+    "revision": "117774f360f3c10b84bdbe4ce0e9fa65",
+    "url": "./static/media/l3_6.117774f3.png"
+  },
+  {
+    "revision": "f164bc4e33f6343e133b57b4f52a8b23",
+    "url": "./static/media/Model_335.f164bc4e.png"
+  },
+  {
+    "revision": "22827fd35cafd23f1c67457217e65a9b",
+    "url": "./static/media/Model_323.22827fd3.png"
+  },
+  {
+    "revision": "a970d805e4b3c6aa96c87db76fa5c351",
+    "url": "./static/media/Model_6041.a970d805.jpg"
+  },
+  {
+    "revision": "448ef18eb2d8022cd312b06226a66681",
+    "url": "./static/media/Model_281.448ef18e.jpg"
+  },
+  {
+    "revision": "c254eaafb689a50bc9dca25bb701385d",
+    "url": "./static/media/Model_1011.c254eaaf.png"
+  },
+  {
+    "revision": "19b43ede85b843a79676d654da4f1217",
+    "url": "./static/media/CO2.19b43ede.png"
+  },
+  {
+    "revision": "f8520c1b9b2cc01141dac7bd6e5da404",
+    "url": "./static/media/ATEX.f8520c1b.png"
+  },
+  {
+    "revision": "7bf16511df5ffaee3d58cb724025b382",
+    "url": "./static/media/High.7bf16511.png"
+  },
+  {
+    "revision": "a8e1082a71827d9ce1d12d194b4a3e58",
+    "url": "./static/media/TEG.a8e1082a.png"
+  },
+  {
+    "revision": "a03b01921796072037d173d968497eca",
+    "url": "./static/media/Model1570.a03b0192.png"
+  },
+  {
+    "revision": "0bbbcb6ff44ab4538980f92b9a0ad751",
+    "url": "./static/media/Model2SF11ES.0bbbcb6f.png"
+  },
+  {
+    "revision": "41b5399508319bfbefb58cf143a6751d",
+    "url": "./static/media/Model2537.41b53995.jpg"
+  },
+  {
+    "revision": "e10850c6e49d4fda66dd30aabc4a7a28",
+    "url": "./static/media/Model3541.e10850c6.png"
+  },
+  {
+    "revision": "5881d927909a9f13f5073c6dfc07f9ae",
+    "url": "./static/media/Model2SF05SEEL.5881d927.jpeg"
+  },
+  {
+    "revision": "3bb26a447af97fbf7680f8643139e860",
+    "url": "./static/media/air_main.3bb26a44.png"
+  },
+  {
+    "revision": "2b01274f979c9a1e6b0c4b2cbb7a8f19",
+    "url": "./static/media/7A-DS-8.2b01274f.jpeg"
+  },
+  {
+    "revision": "616167a84efff73944f0906ae31b8eea",
+    "url": "./static/media/5A-DS-5.616167a8.png"
+  },
+  {
+    "revision": "8e771c55340ecde2d5ed90bcf0d48749",
+    "url": "./static/media/5A-DS-2.8e771c55.jpeg"
+  },
+  {
+    "revision": "d2fa7745e38a3b6ce6bab88bd8c33710",
+    "url": "./static/media/3A-SS-2.5.d2fa7745.jpg"
+  },
+  {
+    "revision": "e96f3046bcc74fdd8a7deb6d18fda253",
+    "url": "./static/media/7L-DD-200.e96f3046.png"
+  },
+  {
+    "revision": "20af489f6c86a44b2de2771eee9bc8fb",
+    "url": "./static/media/5L-SS-45.20af489f.jpeg"
+  },
+  {
+    "revision": "7d343aebf7543c3a3e019f33803b3de5",
+    "url": "./static/media/5L-DS-4.7d343aeb.jpeg"
+  },
+  {
+    "revision": "938e861505a60e778fb27799e4af8b6f",
+    "url": "./static/media/5L-DD-120.938e8615.jpeg"
+  },
+  {
+    "revision": "450f53436604a435b9c8530c933e9e92",
+    "url": "./static/media/3L-SS-8.450f5343.png"
+  },
+  {
+    "revision": "7caadaa2cc30a8b2bdd1637a5e0774a9",
+    "url": "./static/media/Two Stage Series.7caadaa2.png"
+  },
+  {
+    "revision": "ef126c11c0ab7755c04c2da5d806f40f",
+    "url": "./static/media/Single Acting Series.ef126c11.jpeg"
+  },
+  {
+    "revision": "d508518e693e121a7f7cb97b39d4f694",
+    "url": "./static/media/Double Acting Series.d508518e.jpeg"
+  },
+  {
+    "revision": "2201872472738bf5386f4d5bf488bb40",
+    "url": "./static/media/3G-SS-20.22018724.png"
+  },
+  {
+    "revision": "b3755c9b9d79eb5da446e46aa3e1e214",
+    "url": "./static/media/3G-DS-29.b3755c9b.png"
+  },
+  {
+    "revision": "a1f1646b642b1f51469e34c44941af42",
+    "url": "./static/media/logo_white.a1f1646b.png"
+  },
+  {
+    "revision": "3dc17bb6b3c4f4b0a82c1939ee7f6aa8",
+    "url": "./static/media/logo_white.3dc17bb6.png"
+  },
+  {
+    "revision": "fbcadb1af76ed4199c136a6522b88a2d",
+    "url": "./index.html"
+  }
+];
